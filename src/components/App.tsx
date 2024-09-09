f98757423a88ea8f96c0ec3b2016428745ce8c0c
stjernholm <stjernholm@msn.com>
2024-09-09 14:59:16 +0200

    feat: Restructure css files and imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,35 @@
-import { ReactElement } from 'react';
-import './App.css';
-import LandingPage from './LandingPage';
-import Header from './Header';
-import { Route, Routes } from 'react-router-dom';
-import SearchPage from './SearchPage';
-import FavouritesPage from './FavouritesPage';
-import NotFound from './NotFound';
-import CocktailInfoPage from './CocktailInfoPage';
-import IngredientPage from './IngredientPage';
-import Footer from './Footer';
+import { ReactElement } from "react";
+import LandingPage from "./LandingPage";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import SearchPage from "./SearchPage";
+import FavouritesPage from "./FavouritesPage";
+import NotFound from "./NotFound";
+import CocktailInfoPage from "./CocktailInfoPage";
+import IngredientPage from "./IngredientPage";
+import Footer from "./Footer";
 
 export default function App(): ReactElement {
- 
-  return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/favourites" element={<FavouritesPage />} />
-          <Route path="/cocktailinfo/:id" element={<CocktailInfoPage />} />
-          <Route path="/ingredients/:name" element={<IngredientPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/search" element={<SearchPage />} />
+                    <Route path="/favourites" element={<FavouritesPage />} />
+                    <Route
+                        path="/cocktailinfo/:id"
+                        element={<CocktailInfoPage />}
+                    />
+                    <Route
+                        path="/ingredients/:name"
+                        element={<IngredientPage />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+            <Footer />
+        </>
+    );
 }
