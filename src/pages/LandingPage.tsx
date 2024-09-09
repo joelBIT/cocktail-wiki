bced6133363601958847bebd582e932d95ee9bb8
Erik Hallgren <erikhallgren@live.se>
2024-09-09 20:53:32 +0200

    LandingPage placeholder

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,25 @@
 import { ReactElement } from "react";
+import "../css/LandingPage.css";
 
 export function LandingPage(): ReactElement {
-    return <>Landing Page</>;
+	return (
+		<>
+			<h1>Landing Page</h1>
+			<article className="cocktail-card card">
+				<figure className="cocktail-figure">
+					<img className="cocktail-img" src="src/assets/img/cocktail-placeholder.jpg" alt="" />
+					{/* <figcaption>Coctail photo</figcaption> */}
+				</figure>
+				<h3 className="cocktail-card-name">Placeholder name</h3>
+				<section className="cocktail-card-tags">
+					<div className="cocktail-card-tag tag">Expensive</div>
+					<div className="cocktail-card-tag tag">Classic</div>
+				</section>
+				<div className="cocktail-card-buttons">
+					<button className="button-details">Details</button>
+					<button className="button-show-another">Show another</button>
+				</div>
+			</article>
+		</>
+	);
 }
