1aa4364331db84869673d9782baac700c12f6e63
stjernholm <stjernholm@msn.com>
2024-09-10 12:37:08 +0200

    feat: Search function

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,35 @@
-import { ReactElement } from "react";
+import { FormEvent, ReactElement, useState } from "react";
 
 export function SearchPage(): ReactElement {
-    return <>Search Page</>;
+    const [searchDrink, setSearchDrink] = useState<string>("");
+
+    const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(searchDrink);
+        const baseURL = "https://www.thecocktaildb.com/api/json/v1/1";
+        const response = await fetch(`${baseURL}/search.php?s=${searchDrink}`);
+        const data = await response.json();
+        console.log(data);
+        setSearchDrink("");
+    };
+
+    return (
+        <>
+            <form className="form--search-drink" onSubmit={handleOnSubmit}>
+                <label className="label" htmlFor="searchFormInput">
+                    Search for a drink
+                </label>
+                <input
+                    autoFocus
+                    className="input"
+                    id="searchFormInput"
+                    onChange={(e) => setSearchDrink(e.target.value)}
+                    placeholder="I feel like a..."
+                    type="text"
+                    value={searchDrink}
+                />
+                <button className="button">Search</button>
+            </form>
+        </>
+    );
 }
