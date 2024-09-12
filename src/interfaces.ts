5c7420b91cc176b9b8d8abb92356c78419a4cfca
stjernholm <stjernholm@msn.com>
2024-09-12 12:22:42 +0200

    feat: Change IFoundDrink to IDrinkCard interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,4 @@
-export interface IFoundDrink {
+export interface IDrinkCard {
     id: string;
     name: string;
     alcoholic: string;
@@ -6,21 +6,21 @@ export interface IFoundDrink {
 }
 
 export interface ICocktailInformation {
-    drink: string,
-    tags: string[],
-    category: string,
-    glass: string,
-    instructions: string,
-    ingredients: string[],
-    imageAddress: string,
-    measures: string[],
-    amountPerIngredient: string[]
+    drink: string;
+    tags: string[];
+    category: string;
+    glass: string;
+    instructions: string;
+    ingredients: string[];
+    imageAddress: string;
+    measures: string[];
+    amountPerIngredient: string[];
 }
 
 export interface IIngredientInformation {
-    name: string,
-    type: string,
-    alcohol: string,
-    description: string,
-    cocktails: IFoundDrink[]
+    name: string;
+    type: string;
+    alcohol: string;
+    description: string;
+    cocktails: IDrinkCard[];
 }
