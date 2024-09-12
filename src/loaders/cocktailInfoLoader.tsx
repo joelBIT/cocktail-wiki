f378c53c388fa0ae33c783ec70785104e1c8e806
joelBit <joel.rollny@gmail.com>
2024-09-12 15:13:00 +0200

    Code comments

--- src/loaders/cocktailInfoLoader.tsx
+++ src/loaders/cocktailInfoLoader.tsx
@@ -1,6 +1,15 @@
 import { redirect } from "react-router-dom";
 import { ICocktailInformation } from "../interfaces";
 
+/**
+ * Retrieves information about a cocktail from the API. The data of interest is stored in a created object of
+ * type ICocktailInformation. Ingredients and measures are converted to arrays due to the API response lacking in
+ * arrays. The strTags property in the response is a string containing tags separated by commas, hence the use
+ * of split().
+ * 
+ * @param id    the id of the cocktail which has its info retrieved from the API
+ * @returns     an ICocktailInformation object for use in the CocktailInfoPage.
+ */
 export const cocktailInfoLoader = async ({params}: any) => {
 
     try {
@@ -20,20 +29,15 @@ export const cocktailInfoLoader = async ({params}: any) => {
             imageAddress: drink.strDrinkThumb
         };
 
+        // The response contains 30 properties that are pushed into two arrays  (if they are non-null)
         for (const property in drink) {
-            if (property.startsWith('strIngredient') && drink[property]) {
+            if (property.startsWith('strIngredient') && drink[property]) {  // 15 ingredient properties
                 cocktail.ingredients.push(drink[property]);
             }
-            if (property.startsWith('strMeasure') && drink[property]) {
+            if (property.startsWith('strMeasure') && drink[property]) {     // 15 measure properties
                 cocktail.measures.push(drink[property]);
             }
         }
-
-        cocktail.ingredients.forEach((ingredient, index) => {
-            if (cocktail.measures[index]) {
-                cocktail.amountPerIngredient.push(`${cocktail.measures[index]} ${ingredient}`);
-            }
-        });
 
         return cocktail;
     } catch (error) {
