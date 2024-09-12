40de962c30ecbc02a139d4dfdd545329f570ce0c
Erik Hallgren <erikhallgren@live.se>
2024-09-12 15:49:10 +0200

    refactor: 'show another' button functionality, no longer a hard refresh

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,36 +1,43 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { IDrinkCard } from "../interfaces";
 import { DrinkCard } from "../components";
 
 export function LandingPage(): ReactElement {
-    const navigate = useNavigate();
-    const drink = useLoaderData() as IDrinkCard;
+	const navigate = useNavigate();
+	const drink_old = useLoaderData() as IDrinkCard;
+	// const drink2 = useLoaderData() as IDrinkCard;
+	// const [drink, setDrink] = useState<IDrinkCard | undefined>(drink_old);
+	const [drink, setDrink] = useState<IDrinkCard>(drink_old);
 
-    const handleUpdateRandomDrink = () => {
-        navigate(0); //detta triggar loadern för den aktuella sidan
-    };
-    const handleDetailsButton = () => {
-        navigate("/cocktailinfo/" + drink.id);
-    };
+	// fetches a new random drink from API
+	async function fetchNewRandomDrink<IDrinkCard>() {
+		const resp: Response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+		// data massaging
+		const { drinks } = await resp.json();
+		const drink = drinks[0];
+		return {
+			id: drink.idDrink,
+			name: drink.strDrink,
+			alcoholic: drink.strAlcoholic,
+			image: drink.strDrinkThumb,
+		};
+	}
 
-    return (
-        <>
-            <DrinkCard drink={drink} />
-            <div className="cocktail-card-buttons" id="TEST">
-                <button
-                    onClick={handleDetailsButton}
-                    className="button-details"
-                >
-                    Details
-                </button>
-                <button
-                    onClick={handleUpdateRandomDrink}
-                    className="button-show-another"
-                >
-                    Show another
-                </button>
-            </div>
-        </>
-    );
+	const handleUpdateRandomDrink = async () => {
+		// fetches new drink to display
+		const newDrink = await fetchNewRandomDrink<IDrinkCard>();
+		setDrink(newDrink);
+	};
+
+	return (
+		<>
+			<DrinkCard drink={drink} />
+			<div className="cocktail-card-buttons" id="TEST">
+				<button onClick={handleUpdateRandomDrink} className="button-show-another">
+					Show another
+				</button>
+			</div>
+		</>
+	);
 }
