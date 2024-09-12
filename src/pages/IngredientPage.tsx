145373e886c5f238eb723bb191734a68749a7e38
stjernholm <stjernholm@msn.com>
2024-09-12 11:35:08 +0200

    refactor: Remove css imports from pages

--- src/pages/IngredientPage.tsx
+++ src/pages/IngredientPage.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { Link, useLoaderData } from "react-router-dom";
-import "../css/IngredientPage.css";
 import { IIngredientInformation } from "../interfaces";
 
 export function IngredientPage(): ReactElement {
@@ -10,23 +9,35 @@ export function IngredientPage(): ReactElement {
         <>
             <section className="ingredient-information">
                 <article>
-                    <figure> 
-                        <img src={`https://www.thecocktaildb.com/images/ingredients/${ingredient.name}-Medium.png`} />
+                    <figure>
+                        <img
+                            src={`https://www.thecocktaildb.com/images/ingredients/${ingredient.name}-Medium.png`}
+                        />
                     </figure>
-                    <h3>Type: <p>{ingredient.type}</p></h3>
-                    <h3>Contains alcohol: <p>{ingredient.alcohol}</p></h3>
+                    <h3>
+                        Type: <p>{ingredient.type}</p>
+                    </h3>
+                    <h3>
+                        Contains alcohol: <p>{ingredient.alcohol}</p>
+                    </h3>
                     <h3>Drinks containing {ingredient.name}: </h3>
                     <ul>
                         {ingredient.cocktails.map((cocktail, index) => (
                             <li key={index}>
-                                <Link to={`/cocktailinfo/${cocktail.id}`}>{cocktail.name}</Link>
+                                <Link to={`/cocktailinfo/${cocktail.id}`}>
+                                    {cocktail.name}
+                                </Link>
                             </li>
                         ))}
                     </ul>
                 </article>
                 <article className="ingredient-description">
-                    <h1>Ingredient: <p>{ingredient.name}</p></h1>
-                    <h3>Description: <p>{ingredient.description}</p></h3>
+                    <h1>
+                        Ingredient: <p>{ingredient.name}</p>
+                    </h1>
+                    <h3>
+                        Description: <p>{ingredient.description}</p>
+                    </h3>
                 </article>
             </section>
         </>
