1d51d6e520eb364eeb11d1d72e4aca9ac0bc1099
stjernholm <stjernholm@msn.com>
2024-09-13 22:54:39 +0200

    feat: Material Icons for pagination nav

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -13,15 +13,23 @@ export function SearchResult(props: ISearchResultProps) {
     return (
         <article id="searchResult">
             <h2>Search Result</h2>
-            <button id="previousDrinks" onClick={props.handlePreviousDrinks}>
-                Previous
-            </button>
-            <span>
-                {props.currentPage} / {props.totalPages}
-            </span>
-            <button id="nextDrinks" onClick={props.handleNextDrinks}>
-                Next
-            </button>
+            <div id="paginateNav">
+                <button
+                    id="previousDrinks"
+                    onClick={props.handlePreviousDrinks}
+                >
+                    <span className="material-symbols-outlined">
+                        skip_previous
+                    </span>
+                </button>
+                <p>
+                    {props.currentPage} / {props.totalPages}
+                </p>
+                <button id="nextDrinks" onClick={props.handleNextDrinks}>
+                    <span className="material-symbols-outlined">skip_next</span>
+                </button>
+            </div>
+
             <section id="searchCardContainer">
                 {props.paginated.map((drink) => (
                     <DrinkCard key={drink.id} drink={drink} />
