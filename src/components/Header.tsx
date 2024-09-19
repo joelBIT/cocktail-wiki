67a677c1d9ab3c97c270fc4acae3ed8ae6423284
Erik Hallgren <erikhallgren@live.se>
2024-09-19 10:09:41 +0200

    reverted out.-of-scope-changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,34 +2,32 @@ import { ReactElement } from "react";
 import { NavLink } from "react-router-dom";
 
 export function Header(): ReactElement {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink
-                            to="/"
-                            className={({ isActive }) => {
-                                return isActive
-                                    ? "active"
-                                    : "";
-                            }}
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/search">
-                            Search
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/favourites">
-                            Favourites
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<NavLink
+							to="/"
+							className={({ isActive }) => {
+								return isActive ? "active" : "";
+							}}
+						>
+							<h2>Home</h2>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/search">
+							<h2>Search</h2>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/favourites">
+							<h2>Favourites</h2>
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
