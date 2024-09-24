053bb062146946cb14350236360f623136272604
Erik Hallgren <erikhallgren@live.se>
2024-09-24 16:26:46 +0200

    added message to favorites page when empty.

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -3,13 +3,14 @@ import { FavouritesContext } from "../contexts/FavouritesContextProvider";
 import { DrinkCard } from "../components";
 
 export function FavouritesPage(): ReactElement {
-    const { favouritesList, setFavouritesList } = useContext(FavouritesContext);
+	const { favouritesList, setFavouritesList } = useContext(FavouritesContext);
 
-    return (
-        <section id="favouritesPage">
-            {favouritesList.map((favourite) => (
-                <DrinkCard key={favourite.id} drink={favourite} />
-            ))}
-        </section>
-    );
+	return (
+		<section id="favouritesPage">
+			<h1>{favouritesList.length ? "" : "There are currently no favourite drinks to display!"}</h1>
+			{favouritesList.map((favourite) => (
+				<DrinkCard key={favourite.id} drink={favourite} />
+			))}
+		</section>
+	);
 }
