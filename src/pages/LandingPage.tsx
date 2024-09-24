d0af6112f4d18492d6ff7d0cb14c7fb7b9981363
stjernholm <stjernholm@msn.com>
2024-09-24 15:38:30 +0200

    style: Positioning of spinner

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,48 +5,51 @@ import { DrinkCard, Spinner } from "../components";
 import { baseURL, createDrinkCard } from "../utils";
 
 export function LandingPage(): ReactElement {
-	const drink_old = useLoaderData() as IDrinkCard;
-	const [drink, setDrink] = useState<IDrinkCard>(drink_old);
-	const [errorMessage, setErrorMessage] = useState("");
-	const [loading, setLoading] = useState(false);
+    const drink_old = useLoaderData() as IDrinkCard;
+    const [drink, setDrink] = useState<IDrinkCard>(drink_old);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [loading, setLoading] = useState(false);
 
-	// Fetches a new random drink from API
-	async function handleRandomDrinkButton(): Promise<void> {
-		setErrorMessage("");
-		setLoading(true);
+    // Fetches a new random drink from API
+    async function handleRandomDrinkButton(): Promise<void> {
+        setErrorMessage("");
+        setLoading(true);
 
-		try {
-			// Send fetch request
-			const resp: Response = await fetch(`${baseURL}/random.php`);
+        try {
+            // Send fetch request
+            const resp: Response = await fetch(`${baseURL}/random.php`);
 
-			// Data massaging
-			const { drinks } = await resp.json();
+            // Data massaging
+            const { drinks } = await resp.json();
 
-			// Update data to display
-			setLoading(false);
-			setDrink(createDrinkCard(drinks[0]));
-		} catch (e) {
-			console.error(e);
-			setErrorMessage("Could not retrieve another drink");
-			setLoading(false);
-		}
-	}
+            // Update data to display
+            setLoading(false);
+            setDrink(createDrinkCard(drinks[0]));
+        } catch (e) {
+            console.error(e);
+            setErrorMessage("Could not retrieve another drink");
+            setLoading(false);
+        }
+    }
 
-	return (
-		<section id="landingPage">
-			<h1 className="errorMessage">{errorMessage}</h1>
-			{loading ? (
-				<div className="spinner-wrapper">
-					<Spinner />
-				</div>
-			) : (
-				<>
-					<DrinkCard drink={drink} />
-					<button id="randomDrinkButton" onClick={() => handleRandomDrinkButton()}>
-						Show another
-					</button>
-				</>
-			)}
-		</section>
-	);
+    return (
+        <section id="landingPage">
+            <h1 className="errorMessage">{errorMessage}</h1>
+            {loading ? (
+                <div className="spinner-wrapper">
+                    <Spinner />
+                </div>
+            ) : (
+                <>
+                    <DrinkCard drink={drink} />
+                    <button
+                        id="randomDrinkButton"
+                        onClick={() => handleRandomDrinkButton()}
+                    >
+                        Show another
+                    </button>
+                </>
+            )}
+        </section>
+    );
 }
