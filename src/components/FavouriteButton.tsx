f669c9d94646c685c57584feeaae3eee5810e1cd
stjernholm <stjernholm@msn.com>
2024-09-24 11:15:16 +0200

    refactor: Code comments and remove console.log statement

--- src/components/FavouriteButton.tsx
+++ src/components/FavouriteButton.tsx
@@ -3,47 +3,51 @@ import { FavouritesContext } from "../contexts/FavouritesContextProvider";
 import { IDrinkCard } from "../interfaces";
 import { useLocation } from "react-router-dom";
 
-export function FavouriteButton({ drink }: { drink: IDrinkCard }): ReactElement {
+export function FavouriteButton({
+    drink,
+}: {
+    drink: IDrinkCard;
+}): ReactElement {
     const { favouritesList, setFavouritesList } = useContext(FavouritesContext);
     const isFavorite = favouritesList.some((fav) => fav.id === drink.id);
     const location = useLocation();
 
-        // Only used for testing the context
-        function handleFavourites(): void {
-            try {
-                if (!isFavorite) {
-                    // if the current drink is not already in favoritesList, add it.
-                    setFavouritesList([...favouritesList, drink]);
-                } else {
-                    // if it is, remove it.
-                    if (location.pathname === "/favourites") {
-                        console.log(location.pathname);
-    
-                        let fadingFavouritesList = [...favouritesList];
-                        for (let favourite of fadingFavouritesList) {
-                            if (favourite.id === drink.id) {
-                                favourite.isFading = true;
-                            }
+    function handleFavourites(): void {
+        try {
+            if (!isFavorite) {
+                // Add drink to favourites list
+                setFavouritesList([...favouritesList, drink]);
+            } else {
+                // Remove drink from favourites list
+                if (location.pathname === "/favourites") {
+                    // When on the favourites page, fade out the card,
+                    // then remove it from favourites list
+                    let fadingFavouritesList = [...favouritesList];
+                    for (let favourite of fadingFavouritesList) {
+                        if (favourite.id === drink.id) {
+                            favourite.isFading = true;
                         }
-    
-                        setFavouritesList(fadingFavouritesList);
-    
-                        setTimeout(() => {
-                            setFavouritesList(
-                                favouritesList.filter((fav) => fav.id !== drink.id)
-                            );
-                        }, 600);
-                    } else {
+                    }
+
+                    setFavouritesList(fadingFavouritesList);
+
+                    setTimeout(() => {
                         setFavouritesList(
                             favouritesList.filter((fav) => fav.id !== drink.id)
                         );
-                    }
+                    }, 600);
+                } else {
+                    // Otherwise, just remove the card from farourites list
+                    setFavouritesList(
+                        favouritesList.filter((fav) => fav.id !== drink.id)
+                    );
                 }
-            } catch (e) {
-                console.log(e);
             }
+        } catch (e) {
+            console.log(e);
         }
-    
+    }
+
     return (
         <div className="button-fav-wrapper">
             <button
@@ -54,13 +58,9 @@ export function FavouriteButton({ drink }: { drink: IDrinkCard }): ReactElement 
                         : "material-symbols-outlined button-fav button-add"
                 }
                 title={
-                    isFavorite
-                        ? "Remove from favorites"
-                        : "Add to favorites"
+                    isFavorite ? "Remove from favorites" : "Add to favorites"
                 }
-            >
-
-            </button>
+            ></button>
             {/* dynamic button content is handled by :hover and ::after selectors in DrinkCard.css */}
         </div>
     );
