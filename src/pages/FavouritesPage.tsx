c5a584e0ddee18222e49321bdce89c318fee6167
Erik Hallgren <erikhallgren@live.se>
2024-09-25 12:26:14 +0200

    Added a few random messages to empty favourites page

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,13 +1,35 @@
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
 import { FavouritesContext } from "../contexts/FavouritesContextProvider";
 import { DrinkCard } from "../components";
 
 export function FavouritesPage(): ReactElement {
 	const { favouritesList, setFavouritesList } = useContext(FavouritesContext);
+	const [randomMessage, setRandomMessage] = useState<string>("");
+	const [messagesList, setMessagesList] = useState<string[]>([]);
+
+	const getRandomMessage = (): string => {
+		// returns a random message!
+		const messages = [
+			"There are currently no favourite drinks to display! 🍸",
+			"No favourite drinks? How boring. 🍹",
+			"You know, you look like you could use a drink. 🥳",
+			"Pleasche drink reschponschibly! 🍻",
+			"I hope you don't come here for beer recommendations! 🍺",
+			"Have you tried Mojito? 🍸",
+		];
+
+		const index = Math.floor(Math.random() * messages.length);
+		return messages[index];
+	};
+
+	useEffect(() => {
+		// Set the random message on component mount
+		setRandomMessage(getRandomMessage());
+	}, []); // The empty dependency array ensures this runs only on component mount
 
 	return (
 		<section id="favouritesPage">
-			<h1>{favouritesList.length ? "" : "There are currently no favourite drinks to display!"}</h1>
+			<h2>{randomMessage}</h2>
 			{favouritesList.map((favourite) => (
 				<DrinkCard key={favourite.id} drink={favourite} />
 			))}
