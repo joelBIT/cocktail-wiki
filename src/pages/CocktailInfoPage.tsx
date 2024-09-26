d0c2a5f291054328da33666f4bc54610281e9b35
joelBit <joel.rollny@gmail.com>
2024-09-26 14:48:49 +0200

    comments on ingredients page and cocktail info page

--- src/pages/CocktailInfoPage.tsx
+++ src/pages/CocktailInfoPage.tsx
@@ -6,7 +6,9 @@ import { createDrinkCardFromCocktail } from "../utils";
 
 /**
  * The cocktail info page includes information such as drink name, ingredients, and instructions 
- * about how to make the drink.
+ * about how to make the drink. The ingredient list contains the measure of each ingredient. By
+ * clicking on the ingredient name the user is redirected to that ingredient's page. It is also
+ * possible to add the cocktail to the list of favourite cocktails by clicking on the Favourites button.
  * 
  * @returns the cocktail info page displaying information about a specific cocktail
  */
